a2b3afc9c841d2db36bd87a1ee0340dab153156f
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-01-03 11:30:31 +0200

    0.2b

--- context/context.js
+++ context/context.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { ethers } from "ethers";
 import toast from "react-hot-toast";
 import JSBI from "jsbi";
-import Web3Modal from web3modal;
+import Web3Modal from "web3modal";
 // internal import uniswap
 import { SwapRouter } from "@uniswap/universal-router-sdk";
 import {
@@ -22,34 +22,34 @@ import {
   Trade as V3Trade,
   Route as RouteV3,
 } from "@uniswap/v3-sdk";
-import { MixedRouteTrade, Trade as RouterTrade } from "@uniswaprouter-sdk";
+import { MixedRouteTrade, Trade as RouterTrade } from "@uniswap/router-sdk";
 import IUniswapV3Pool from "@uniswap/v3-core/artifacts/contracts/UniswapV3Pool.sol/UniswapV3Pool.json";
-//internal import
-import { ERC20_ABI, web3Provider, CONNECTING_CONTRACT } from "./constans";
+// internal import
+import { ERC20_ABI, web3Provider, CONNECTING_CONTRACT } from "./constants";
 import { shortenAddress, parseErrorMsg } from "../utils/index";
 
 export const CONTEXT = React.createContext();
 
-export const PROVIDER = ({children}) => {
+export const PROVIDER = ({ children }) => {
   const TOKEN_SWAP = "JOKER TOKEN SWAP";
   const [loader, setLoader] = useState(false);
   const [address, setAddress] = useState("");
   const [chainID, setChainID] = useState();
 
-  const notifyError = (msg) => toast.error(msg, {duration: 4000});
-  const notifySuccess = (msg) => toast.success(msg, {duration: 4000});
+  const notifyError = (msg) => toast.error(msg, { duration: 4000 });
+  const notifySuccess = (msg) => toast.success(msg, { duration: 4000 });
 
-  //connect wallet function
-  const connect = async() => {
+  // connect wallet function
+  const connect = async () => {
     try {
-      if(!indow.ethereum) return notifyError("Install MetaMask");
+      if (!window.ethereum) return notifyError("Install MetaMask");
 
       const accounts = await window.ethereum.request({
         method: "eth_requestAccounts",
       });
 
       if (accounts.length) {
-        setAddress(accounts[0]); 
+        setAddress(accounts[0]);
       } else {
         notifyError("Sorry, you have no account");
       }
@@ -64,8 +64,8 @@ export const PROVIDER = ({children}) => {
     }
   };
 
-  //load token data
-  const LOAD_TOKEN = async(token) => {
+  // load token data
+  const LOAD_TOKEN = async (token) => {
     try {
       const tokenDetail = await CONNECTING_CONTRACT(token);
       return tokenDetail;
@@ -76,46 +76,54 @@ export const PROVIDER = ({children}) => {
     }
   };
 
-  //internal
+  // internal
   async function getPool(tokenA, tokenB, feeAmount, provider) {
-    const [token0, token1] = tokenA.sortsBefore(tokenB) 
-      ? [tokenA, tokenB] : [tokenB, tokenA];
-    
-    const poolAddress= Pool.getAddress(token0, token1, feeAmount);
-    const contract = new ethers.Contract(poolAddress, IUniswapV3Pool, provider);
+    const [token0, token1] = tokenA.sortsBefore(tokenB)
+      ? [tokenA, tokenB]
+      : [tokenB, tokenA];
+
+    const poolAddress = Pool.getAddress(token0, token1, feeAmount);
+    const contract = new ethers.Contract(poolAddress, IUniswapV3Pool.abi, provider);
     let liquidity = await contract.liquidity();
-    let { sqrtPriceX96 , tick } = await contract.slot0();
+    let { sqrtPriceX96, tick } = await contract.slot0();
 
     liquidity = JSBI.BigInt(liquidity.toString());
     sqrtPriceX96 = JSBI.BigInt(sqrtPriceX96.toString());
-    console.log("CALLAING_POOL------");
 
     return new Pool(
-      token0, token1, feeAmount, sqrtPriceX96, 
-      liquidity, tick, [{
-        index: nearestUsableTick(TickMath.MIN_TICK, TICK_SPACINGS[feeAmount]),
-        liquidityNet: liquidity,
-        liquidityGross: liquidity 
-      }, {
-        index: nearestUsableTick(TickMath.MIN_TICK, TICK_SPACINGS[feeAmount]),
-        liquidiyNet: JSBI.multiply(liquidity, JSBI.BigInt("-1")),
-        liquidityGross: liquidity
-      }
-    ])
+      token0,
+      token1,
+      feeAmount,
+      sqrtPriceX96,
+      liquidity,
+      tick,
+      [
+        {
+          index: nearestUsableTick(TickMath.MIN_TICK, TICK_SPACINGS[feeAmount]),
+          liquidityNet: liquidity,
+          liquidityGross: liquidity,
+        },
+        {
+          index: nearestUsableTick(TickMath.MIN_TICK, TICK_SPACINGS[feeAmount]),
+          liquidityNet: JSBI.multiply(liquidity, JSBI.BigInt("-1")),
+          liquidityGross: liquidity,
+        },
+      ]
+    );
   }
 
-  //swap option function internal
+  // swap option function internal
   function swapOptions(options) {
-    return Object.assing(
+    return Object.assign(
       {
-        slippageTolerance: new Percent(5, 1000),
-        recipient: RECEIPIENT,
+        slippageTolerance: new Percent(JSBI.BigInt(5).toNumber(), JSBI.BigInt(100).toNumber()),
+        recipient: RECIPIENT,
       },
       options
     );
   }
 
-  //build trade
+  // build trade
   function buildTrade(trades) {
     return new RouterTrade({
       v2Routes: trades
@@ -135,32 +143,34 @@ export const PROVIDER = ({children}) => {
         })),
 
       mixedRoutes: trades
-        .filter((trade) => trade instanceof V3Trade)
+        .filter((trade) => trade instanceof MixedRouteTrade)
         .map((trade) => ({
           mixedRoute: trade.route,
-          inputAmount: tradeinputAmount,
+          inputAmount: trade.inputAmount,
           outputAmount: trade.outputAmount,
         })),
-        tradeType: trades[0].tradeType,
+      tradeType: trades[0].tradeType,
     });
   }
 
-  //demo account 
+  // demo account
   const RECIPIENT = "0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B";
 
-  //swap 
-  const swap = async(token_1, token_2, swapInputAmount) => {
+  // swap
+  const swap = async (token_1, token_2, swapInputAmount) => {
     try {
       console.log("CALLING ME______SWAP");
-      const _inputAmount = 1;
       const provider = web3Provider();
 
       const network = await provider.getNetwork();
-      // const ETHER = Ether.onChain(network.chainID)
-      const ETHER = Ether.onChain(1);
+      const ETHER = Ether.onChain(network.chainId);
 
-      const tokenAddress1 = await CONNECTING_CONTRACT("0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2");
-      const tokenAddress2 = await CONNECTING_CONTRACT("0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48");
+      const tokenAddress1 = await CONNECTING_CONTRACT(
+        "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
+      );
+      const tokenAddress2 = await CONNECTING_CONTRACT(
+        "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"
+      );
 
       const TOKEN_A = new Token(
         tokenAddress1.chainId,
@@ -179,21 +189,23 @@ export const PROVIDER = ({children}) => {
       );
 
       const WETH_USDC_V3 = await getPool(
-        TOKEN_A, TOKEN_B, FeeAmount.MEDIUM, provider
+        TOKEN_A,
+        TOKEN_B,
+        FeeAmount.MEDIUM,
+        provider
       );
 
       const inputEther = ethers.utils.parseEther("1");
 
       const trade = await V3Trade.fromRoute(
-        new RouteV3([WETH_USDC_V3, ETHER, TOKEN_B]),
-        CurrencyAmount.fromRawAmount(Ether, inputEther),
+        new RouteV3([WETH_USDC_V3], ETHER, TOKEN_B),
+        CurrencyAmount.fromRawAmount(ETHER, JSBI.BigInt(inputEther.toString())),
         TradeType.EXACT_INPUT
       );
 
       const routerTrade = buildTrade([trade]);
       const opts = swapOptions({});
-      const params = SwapRouter.swapERC20CallParameters(routerTrade, opts);
-      // const params = SwapRouter.swapCallParameters(routerTrade, opts);
+      const params = SwapRouter.swapCallParameters(routerTrade, opts);
       console.log(WETH_USDC_V3);
       console.log(trade);
       console.log(routerTrade);
@@ -205,17 +217,17 @@ export const PROVIDER = ({children}) => {
       let tokenB;
 
       ethBalance = await provider.getBalance(RECIPIENT);
-      tokenA = await tokenAddress1.balance;
-      tokenA = await tokenAddress2.balance;
-      // tokenB = await tokenAddress2.balance;
+      tokenA = await tokenAddress1.balanceOf(RECIPIENT);
+      tokenB = await tokenAddress2.balanceOf(RECIPIENT);
       console.log("____________BEFORE");
       console.log("EthBalance:", ethers.utils.formatUnits(ethBalance, 18));
       console.log("tokenA:", tokenA);
       console.log("tokenB:", tokenB);
 
-      const tx = await sigher.sendTransaction({
+      const signer = await provider.getSigner();
+      const tx = await signer.sendTransaction({
         data: params.calldata,
-        to: "0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B",
+        to: "0xEf1c6E67703c7BD7107eed8303Fbe6EC2554BF6B",
         value: params.value,
         from: RECIPIENT,
       });
@@ -227,9 +239,8 @@ export const PROVIDER = ({children}) => {
       console.log("STATUS", receipt.status);
 
       ethBalance = await provider.getBalance(RECIPIENT);
-      tokenA = await tokenAddress1.balance;
-      tokenA = await tokenAddress2.balance;
-      tokenB = await tokenAddress2.balance;
+      tokenA = await tokenAddress1.balanceOf(RECIPIENT);
+      tokenB = await tokenAddress2.balanceOf(RECIPIENT);
       console.log("-------After");
 
       console.log("EthBalance:", ethers.utils.formatUnits(ethBalance, 18));
@@ -243,7 +254,7 @@ export const PROVIDER = ({children}) => {
   };
 
   return (
-    <CONTEXT.Provider 
+    <CONTEXT.Provider
       value={{
         TOKEN_SWAP,
         LOAD_TOKEN,
@@ -258,8 +269,7 @@ export const PROVIDER = ({children}) => {
     >
       {children}
     </CONTEXT.Provider>
-  )
-
+  );
 };
 
    
