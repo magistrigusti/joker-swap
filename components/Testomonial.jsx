dab00d0bb7ce201bfa198ce2f507d836616f2dc6
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-01-08 09:46:36 +0200

    0.6

--- components/Testomonial.jsx
+++ components/Testomonial.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaRegStar } from "react-icons/fa";
 
 const Testomonial = () => {
   return (
@@ -34,20 +35,156 @@ const Testomonial = () => {
 
               <div className="client one">
                 <div className="img one"
-                  style={{backgroundImage: "url('assets/img/testi1.png')"}}
+                  style={{ backgroundImage: "url('assets/img/testi1.png')" }}
                 >
-                  <div id="myPopover"
-                    className="popover popover-detault mypopover"
-                  >
+                  <div className="popover popover-default mypopover" id="myPopover">
                     <div className="client-review">
                       <div className="stars">
-                        {[1, 2, 3, 4, 5].map((item, index) => {
-                          <i className="fas fa-star"></i>
-                        })}
+                        {[1, 2, 3, 4, 5].map((item, index) => (
+                          <i className="fas " key={index}>
+                            <FaRegStar />
+                          </i>
+                        ))}
                       </div>
+
 
                       <p className="bottom-text">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      </p>
+
+                      <div className="client-info">
+                        <h4 className="name">Flora Oliver</h4>
+                        <p className="position">CEO & Founder</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="client two">
+                <div className="img two"
+                  style={{ backgroundImage: "url('assets/img/testi2.png')" }}
+                >
+                  <div className="popover popover-default mypopover" id="myPopover2">
+                    <div className="client-review">
+                      <div className="stars">
+                        {[1, 2, 3, 4, 5].map((item, index) => (
+                          <i className="fas " key={index}>
+                            <FaRegStar />
+                          </i>
+                        ))}
+                      </div>
+
+                      <p className="bottom-text">
+                        Lorem ipsum doloe sit amet consectetur abipisicing elit.
+                      </p>
+
+                      <div className="client-info">
+                        <h4 className="name">Flora Oliver</h4>
+                        <p className="position">CEO & Founder</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="client three">
+                <div className="img three"
+                  style={{ backgroundImage: "url('assets/img/testi3.png')" }}
+                >
+                  <div className="popover popover-default mypopover" id="myPopover3">
+                    <div className="client-review">
+                      <div className="stars">
+                        {[1, 2, 3, 4, 5,].map((item, index) => (
+                          <i className="fas " key={index}>
+                            <FaRegStar />
+                          </i>
+                        ))}
+                      </div>
+
+                      <p className="bottom-text">
+                        Lorem ipsum dolor sit amet conectetur abipisicing elit.
+                      </p>
+
+                      <div className="client-info">
+                        <h4 className="name">Flora Oliver</h4>
+                        <p className="position">CEO & Founder</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="client four">
+                <div className="img four"
+                  style={{backgroundImage: "url('assets/img/testi4.png')"}}
+                >
+                  <div className="popover popover-default mypopover" id="myPopover4">
+                    <div className="client-review">
+                      <div className="stars">
+                        {[1, 2, 3, 4, 5].map((item, index) => (
+                          <i className="fas" key={index}>
+                            <FaRegStar />
+                          </i>
+                        ))}
+                      </div>
+
+                      <p className="bottom-text">
+                        Lorem ipsum dolor sit amet consectetur abipisicing elit.
+                      </p>
+
+                      <div className="client-info">
+                        <h4 className="name">Flora Oliver</h4>
+                        <p className="position">CEO & Founder</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="client five">
+                <div className="img five"
+                  style={{backgroundImage: "url('assets/img/testi5.png')"}}
+                >
+                  <div className="popover popover-default mypopover" id="myPopover5">
+                    <div className="client-review">
+                      <div className="stars">
+                        {[1, 2, 3, 4, 5].map((item, index) => (
+                          <i className="fas" key={index}>
+                            <FaRegStar />
+                          </i>
+                        ))}
+                      </div>
+
+                      <p className="bottom-text">
+                        Lorem ipsum dolor sit amet consectetur abipiicing elit.
+                      </p>
+
+                      <div className="client-info">
+                        <h4 className="name">Flora Oliver</h4>
+                        <p className="position">CEO & Founder</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="client six">
+                <div className="img six"
+                  style={{backgroundImage: "url('assets/img/testi6.png')"}}
+                >
+                  <div className="popover popover-default mypopover" id="myPopover6">
+                    <div className="client-review">
+                      <div className="stars">
+                        {[1, 2, 3, 4, 5].map((item, index) => (
+                          <i className="fas" key={index}>
+                            <FaRegStar />
+                          </i>
+                        ))}
+                      </div>
+
+                      <p className="bottom-text">
+                        Lorem ipsum dolor sit amet consectetur abipiicing elit.
                       </p>
 
                       <div className="client-info">
--- components/Token.jsx
+++ components/Token.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useEffect, useState   } from "react";
+import { shortenAddress } from "../utils/index";
 
-const Token = () => {
+const Token = (
+  notifyError,
+  notifySuccess,
+  setOpenToken,
+  LOAD_TOKEN,
+  setToken_1,
+  setToken_2,
+  token_1,
+  token_2
+) => {
+  const [searchToken, setSearchToken] = useState();
+  const [displayToken, setDisplayToken] = useState();
+
+  useEffect(() => {
+    const loadToken = async() => {
+      const token = await LOAD_TOKEN(searchToken);
+
+      if (token == undefined) {
+        notifyError("Token address is missing");
+      } else {
+        setDisplayToken(token);
+      }
+    };
+
+    loadToken();
+  }, [searchToken]);
+
+  const selectToken = () => {
+    if (token_1 == undefined) {
+      setToken_1(displayToken)
+    }
+  }
+
   return <div>Token</div>;
 };
 
 export default Token;
+
